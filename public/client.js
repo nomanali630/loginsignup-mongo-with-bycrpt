8ca6256e0a4fc1ae2eb34f125379cae2e701fac3
nomanali630 <hussainkhaan78@gmail.com>
2021-01-13 15:00:42 +0500

    123

--- public/client.js
+++ public/client.js
@@ -9,9 +9,32 @@ function signup() {
             phone: document.getElementById('phone').value,
             gender: document.getElementById('gender').value,
         }
+    })
+        .then((response) => {
+            console.log(response);
+            // alert(response.data.message)
+        })
+        .catch((err) => {
+            console.log("Error ", err);
+            // alert(response.data.message)
+        })
+    return false;
+}
+function login() {
+    axios({
+        method: 'post',
+        url: 'http://localhost:5000/login',
+        withCredentials: true,
+        data: {
+
+            email: document.getElementById('email12').value,
+            password: document.getElementById('password12').value,
+
+        }
     }).then((response) => {
         console.log(response);
         alert(response.data.message)
+        window.location.href = "./login.html"
     }, (error) => {
         console.log(error);
     });
